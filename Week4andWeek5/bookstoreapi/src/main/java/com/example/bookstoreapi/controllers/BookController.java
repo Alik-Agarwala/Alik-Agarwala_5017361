771c1386cd5ba3e69157779ae18c060ac35f0091
Alik Agarwala <alikagarwala@outlook.com>
2024-08-21 13:55:34 +0530

    Implemented DTOs and mapping for Book entity

--- Week4andWeek5/bookstoreapi/src/main/java/com/example/bookstoreapi/controllers/BookController.java
+++ Week4andWeek5/bookstoreapi/src/main/java/com/example/bookstoreapi/controllers/BookController.java
@@ -1,15 +1,16 @@
 package com.example.bookstoreapi.controllers;
 
+import com.example.bookstoreapi.dto.BookDTO;
+import com.example.bookstoreapi.dto.BookMapper;
 import com.example.bookstoreapi.exception.ResourceNotFoundException;
 import com.example.bookstoreapi.model.Book;
 import com.example.bookstoreapi.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/books")
@@ -19,32 +20,36 @@ public class BookController {
     private BookRepository bookRepository;
 
     @GetMapping
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
+    public List<BookDTO> getAllBooks() {
+        return bookRepository.findAll().stream()
+                .map(BookMapper.INSTANCE::toBookDTO)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
-        Book book = bookRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Book not found with id " + id));
-        return ResponseEntity.ok(book);
-    }
+public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
+    Book book = bookRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Book not found with id " + id));
+    return ResponseEntity.ok(BookMapper.INSTANCE.toBookDTO(book));
+}
+
 
     @PostMapping
-    public Book createBook(@RequestBody Book book) {
-        return bookRepository.save(book);
+    public BookDTO createBook(@RequestBody BookDTO bookDTO) {
+        Book book = BookMapper.INSTANCE.toBook(bookDTO);
+        return BookMapper.INSTANCE.toBookDTO(bookRepository.save(book));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book bookDetails) {
+    public ResponseEntity<BookDTO> updateBook(@PathVariable Long id, @RequestBody BookDTO bookDTO) {
         Book book = bookRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Book not found with id " + id));
 
-        book.setTitle(bookDetails.getTitle());
-        book.setAuthor(bookDetails.getAuthor());
-        book.setPrice(bookDetails.getPrice());
-        book.setIsbn(bookDetails.getIsbn());
-        return ResponseEntity.ok(bookRepository.save(book));
+        book.setTitle(bookDTO.getTitle());
+        book.setAuthor(bookDTO.getAuthor());
+        book.setPrice(bookDTO.getPrice());
+        book.setIsbn(bookDTO.getIsbn());
+        return ResponseEntity.ok(BookMapper.INSTANCE.toBookDTO(bookRepository.save(book)));
     }
 
     @DeleteMapping("/{id}")
